cba182816123844fe897d6fda9e0d6b9624cefe3
Pablo <pablom1206@outlook.es>
2025-03-21 16:45:23 -0400

    se arreglo el estilos.css

--- js/random.js
+++ js/random.js
@@ -2,16 +2,18 @@ const themeLink = document.getElementById('theme');
 const styles = ['/css/estilos.css', '/css/estilos-urbano.css', '/css/estilos-futuro.css'];
 let currentIndex = 0;
 
-document.getElementById('estiloLineal').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % styles.length; // 
+// Cambiar estilos de forma lineal
+document.getElementById('toggleLinear').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % styles.length; 
     themeLink.setAttribute('href', styles[currentIndex]);
 });
 
-document.getElementById('estiloAleatorio').addEventListener('click', () => {
+// Cambiar estilos de forma aleatoria
+document.getElementById('toggleRandom').addEventListener('click', () => {
     let randomIndex;
     do {
         randomIndex = Math.floor(Math.random() * styles.length); 
-  } while (randomIndex === currentIndex); // 
+    } while (randomIndex === currentIndex); 
     currentIndex = randomIndex;
     themeLink.setAttribute('href', styles[currentIndex]);
 });
